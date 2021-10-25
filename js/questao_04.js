b5573e8f37030f8e78422ad6681e9b0bb46d9151
Thiago da Silva Ferreira <72448615+ThiiFerreira@users.noreply.github.com>
2021-10-25 15:53:05 +0000

    atualizando o js_questao-04 colando funções de exemplo no código

--- js/questao_04.js
+++ js/questao_04.js
@@ -21,16 +21,23 @@ class Aluno {
   }
 }
 
-var alunos = new Array();
-alunos[0] = new Aluno("Thiago", "Ferreira", 10, 10);
-alunos[1] = new Aluno("Igor", "Silva", 9, 8);
-alunos[2] = new Aluno("Luan", "Junior", 10, 10);
-alunos[3] = new Aluno("Maria", "Jose", 5, 4);
-alunos[4] = new Aluno("Jose", "Maria", 6, 3);
+function criarAlunos(){
+  var alunos = new Array();
+  alunos[0] = new Aluno("Thiago", "Ferreira", 10, 10);
+  alunos[1] = new Aluno("Igor", "Silva", 9, 8);
+  alunos[2] = new Aluno("Luan", "Junior", 10, 10);
+  alunos[3] = new Aluno("Maria", "Jose", 5, 4);
+  alunos[4] = new Aluno("Jose", "Maria", 6, 3);
+  return alunos
+}
 
-
-for (var i = 0 ; i<alunos.length ; i++){
+function mostrarAlunos(alunos){
+  for (var i = 0 ; i<alunos.length ; i++){
   alert("Nome completo: " + alunos[i].nomeCompleto() + "\n" +
   "Media: " + alunos[i].media() + "\n" +
   "Situacao: " + alunos[i].situacao());
+  }
 }
+
+alunos = criarAlunos();
+mostrarAlunos(alunos);
